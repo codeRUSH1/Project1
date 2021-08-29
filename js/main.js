6b5c9d5e639889d7cf029bd3e0121ea387d9a07c
Jakub K <60154750+codeRUSH1@users.noreply.github.com>
2021-08-29 20:23:46 +0100

    pushing some commits

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
+function init() {
+
 // Global variables
+const grid = document.querySelector('grid')
+const width = 9
+const cellSize = width * width 
+const cells = []
+const startingPosition = 0
+
+function gridCreation() {
+    for (let i = 0; i < cellSize; i++) {
+        const cell = document.createElement('div')
+        cell.innerHTML = i
+        grid.appendChild(cell)
+        cells.push(cell)
+    }
+}
+
+gridCreation()
+
+
+
 
 // Start button for the game, which will have an event listener 'click'
-const startGameButton = document.getElementById()
+
 
 // Will have the gun spawn at the centre of the bottom row on the grid, may change it to a random spawn point in the bottom row though later on.
-const gunStartingPosition = document.getElementById()
+
 
 // Player score should be global, and always start at 0
 const playerScore = 0
@@ -18,7 +39,7 @@ const PlayerLives = 3
 0.1 During this time, the 'aliens' should spawn randomly distributed, alongside the mounted gun turret spawning at the bottom.
 
 1. Once the above countdown/timer/interval is over, it should be possible for the alien armada to move, and also possible for the mounted gun to move.
-- Mounted gun should be able to shoot projectiless, and move from left to right alongside the grid. it is not able to move vertically though.
+- Mounted gun should be able to shoot projectiles, and move from left to right alongside the grid. it is not able to move vertically though.
 - Alien armada is able to move both horizontally and then vertically, as it drops lower, the speed of moving vertically and horizontally gradually increments/increases.
 
 2. Projectiles
@@ -31,5 +52,10 @@ OR
 */
 
 /* 
-BONUS
+BONUS 
+
 */
+
+}
+
+window.addEventListener('DOMContentLoaded', init)
